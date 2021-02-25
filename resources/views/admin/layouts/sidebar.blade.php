707d93780c509365ad3e0fe2ee332e453b5cf16a
Yunus <yunusadbulqudus1@gmail.com>
2021-02-25 15:57:14 +0100

    updated readme

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -17,18 +17,9 @@
             <li class="{{$base_url . '/admin/admins' === $current_url ? 'active' : ''}}"><a class="nav-link" href="{{route('admins.index')}}"><i class="fas fa-users"></i> <span>Admins</span></a></li>
             <li class="{{$base_url . '/admin/admins/create' === $current_url ? 'active' : ''}}"><a class="nav-link" href="{{route('admins.create')}}"><i class="fas fa-user-plus"></i> <span>Add Admin</span></a></li>
 
-            {{--<li class="nav-item dropdown active">--}}
-                {{--<a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>--}}
-
-                {{--<ul class="dropdown-menu">--}}
-                    {{--<li><a class="nav-link" href="index-0.html">General Dashboard</a></li>--}}
-                    {{--<li class="active"><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>--}}
-                {{--</ul>--}}
-            {{--</li>--}}
             <li class="menu-header">Starter</li>
             <li class="{{$base_url . '/admin/users' === $current_url ? 'active' : ''}}"><a class="nav-link" href="{{route('users.index')}}"><i class="fas fa-user"></i> <span>Users</span></a></li>
 
-            {{--<li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>--}}
             <li class="menu-header">AUTHORIZING</li>
             <li class="{{$base_url . '/admin/role' === $current_url ? 'active' : ''}}"><a class="nav-link" href="{{route('role.index')}}"><i class="fas fa-pencil-ruler"></i> <span>Roles</span></a></li>
             <li class="{{$base_url . '/admin/permission' === $current_url ? 'active' : ''}}"><a class="nav-link" href="{{route('permission.index')}}"><i class="fas fa-pencil-ruler"></i> <span>Permissions</span></a></li>
